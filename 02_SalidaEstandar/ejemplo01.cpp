8e9aa05a7f8839e02e346ef0247866a1664d419f
Uriel Haile <perenganito@gmail.com>
2021-08-27 13:31:18 -0500

    Ejemplos

---
+++ 02_SalidaEstandar/ejemplo01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// ASCII 
+int main() {
+    std::cout << "Uriel Haile Hernández Belmonte" << std::endl;
+    std::cout << "M" << std::endl;
+    std::cout << "u" << std::endl;
+    std::cout << "n" << std::endl;
+    std::cout << "d" << std::endl;
+    std::cout << "o" << std::endl;
+}
---
+++ 03_Variables/ejemplo02.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    //char a;
+    //std::cout << "Teclea un caracter" << std::endl;
+    //std::cin >> a;
+    //std::cout << "El caracter: "<< a << std::endl;
+
+    /* 
+    int num;
+    std::cout << "Teclea un numero entero" << std::endl;
+    std::cin >> num;
+    std::cout << "El numero: "<< num << std::endl;
+    */
+
+    /*
+    float numf;
+    std::cout << "Teclea un numero flotante" << std::endl;
+    std::cin >> numf;
+    std::cout << "El numero: "<< numf << std::endl;
+    */
+    
+    double numd;
+    std::cout << "Teclea un numero double" << std::endl;
+    std::cin >> numd;
+    std::cout << "El numero: "<< numd << std::endl;
+} 
