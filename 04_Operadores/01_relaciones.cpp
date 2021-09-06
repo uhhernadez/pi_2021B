c1b30c4f1a7c59045d75e987d259e9b791fa1008
Uriel Haile <perenganito@gmail.com>
2021-09-06 07:18:34 -0500

    Operadores sesión 4

---
+++ 04_Operadores/01_relaciones.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; // Utilice el espacio de nombres std, estándar
+int main() {
+    int a = 10;
+    int b = 3;
+    cout << " ( a > b ) " << (a > b) <<  endl;
+    cout << " ( a < b ) " << (a < b) <<  endl;
+    cout << " ( a == b )" << (a == b) << endl;
+    cout << " ( a != b )" << (a != b) << endl;
+    cout << " ( a >= b )" << (a >= b) << endl;
+    cout << " ( a <= b )" << (a <= b) << endl;
+}
---
+++ 04_Operadores/02_OperadorAgrupacion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 3, b = 7, c = 11;
+    int oper = 10 * (a + b +c);
+    cout << "Resultado\t " << oper << endl;
+    cout << "Resultado\t " << oper << "\t";
+    cin >> a >> b >> c;
+    // 67 89 09
+}
